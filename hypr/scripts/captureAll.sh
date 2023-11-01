67dd5792c3bc8af221cd87ab7e54adc1e1997f60
Lucas <lucas.verbeiren@gmail.com>
2023-11-01 15:48:54 +0100

    corrected captureAll.sh script

--- hypr/scripts/captureAll.sh
+++ hypr/scripts/captureAll.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#grim /home/lulu/Pictures/screenshots/$(date +'%s_grim.png')
-
-grim png | wl-copy && wl-paste > /home/lulu/Pictures/screenshots/$(date +'%s_grim.png')
-
+grim /home/lulu/Pictures/screenshots/$(date +'%s_grim.png')
