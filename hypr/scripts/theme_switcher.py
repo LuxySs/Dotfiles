9b406ffd6b257d6815c4ebf36b27e528a63ab45e
Lucas <lucas.verbeiren@gmail.com>
2023-11-08 21:50:56 +0100

    modified code to work with any username for home/username directory

--- hypr/scripts/theme_switcher.py
+++ hypr/scripts/theme_switcher.py
@@ -2,12 +2,15 @@ import os
 import subprocess as sp
 import time
 
+#home directory ("/" not included)
+homedir = os.path.expanduser('~')[1:]
+print(homedir)
 #list of all themes
-all_themes = os.listdir("/home/lulu/.config/hypr/conf/themes")
+all_themes = os.listdir(f"/{homedir}/.config/hypr/conf/themes")
 print(all_themes)
 
 #full path of the symlink
-syml = os.readlink("/home/lulu/.config/hypr/conf/window.conf")
+syml = os.readlink(f"/{homedir}/.config/hypr/conf/window.conf")
 print(syml)
 
 #file name of the theme used
@@ -20,16 +23,16 @@ print(current_theme_index)
 
 #name of the next theme
 if current_theme_index == len(all_themes) - 1:
-    next_theme = f"/home/lulu/.config/hypr/conf/themes/{all_themes[0]}"
+    next_theme = f"/{homedir}/.config/hypr/conf/themes/{all_themes[0]}"
 else:
-    next_theme = f"/home/lulu/.config/hypr/conf/themes/{all_themes[current_theme_index + 1]}"
+    next_theme = f"/{homedir}/.config/hypr/conf/themes/{all_themes[current_theme_index + 1]}"
 print(next_theme)
 
 #delete current symbolic (linked) file
-os.remove("/home/lulu/.config/hypr/conf/window.conf")
+os.remove(f"/{homedir}/.config/hypr/conf/window.conf")
 
 #create new symbolic link to next_theme
-os.symlink(next_theme, "/home/lulu/.config/hypr/conf/window.conf")
+os.symlink(next_theme, f"/{homedir}/.config/hypr/conf/window.conf")
 
 #print(all_themes)
 #print(syml)
