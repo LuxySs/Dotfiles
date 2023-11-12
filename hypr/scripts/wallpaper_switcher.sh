7ea177255a3e33b18be9fe9cd04e6577dede1b1d
Lucas <lucas.verbeiren@gmail.com>
2023-11-12 12:42:08 +0100

    updated keybindings to not use scripts when unnecessary (rofi and spotify+cava) + added wallpaper_switcher.sh (bof bof)

---
+++ hypr/scripts/wallpaper_switcher.sh
@@ -0,0 +1,4 @@
+:whyprctl hyprpaper wallpaper "DP-1,~/Dotfiles/wallpapers/gas_station.jpg" & hyprctl hyprpaper wallpaper "DP-2,~/Dotfiles/wallpapers/gas_station.jpg"
+
+hyprctl hyprpaper wallpaper "DP-1,~/Dotfiles/wallpapers/lake_moutains.jpg" & hyprctl hyprpaper wallpaper "DP-2,~/Dotfiles/wallpapers/lake_moutains.jpg"
+
