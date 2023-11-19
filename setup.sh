507219c550f5ad64620615f046958ccb7839e21a
Lucas <lucas.verbeiren@gmail.com>
2023-11-19 18:11:12 +0100

    ameliorations to setup.sh (might not work still)

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ ask_platform () {
     echo "laptop or desktop ?"
     read computer_type
     if [ "${computer_type}" = "laptop" ]; then
-        cp "${dotfiles_dir/hypr/laptop_hyprland.conf}" "${dotfiles_dir/hypr/hyprland.conf}"
-        cp "${dotfiles_dir/hypr/laptop_hyprpaper.conf}" "{$dotfiles_dir/hypr/hyprpaper.conf}"
+        cp "${dotfiles_dir}/hypr/laptop_hyprland.conf" "${dotfiles_dir}/hypr/hyprland.conf"
+        cp "${dotfiles_dir}/hypr/laptop_hyprpaper.conf" "${dotfiles_dir}/hypr/hyprpaper.conf"
     elif [ "${computer_type}" = "desktop" ]; then
-        cp "${dotfiles_dir/hypr/desktop_hyprland.conf}" "${dotfiles_dir/hypr/hyprland.conf}"
-        cp "${dotfiles_dir/hypr/desktop_hyprpaper.conf}" "${dotfiles_dir/hypr/hyprpaper.conf}"
+        cp "${dotfiles_dir}/hypr/desktop_hyprland.conf" "${dotfiles_dir}/hypr/hyprland.conf"
+        cp "${dotfiles_dir}/hypr/desktop_hyprpaper.conf" "${dotfiles_dir}/hypr/hyprpaper.conf"
     else
         return 1
     fi
@@ -33,6 +33,6 @@ mkdir "${backup_dir}" # create backup directory
 
 #move previous configs to backup directory
 for f in alacritty cava fish hypr neofetch rofi starship.toml waybar wlogout; do
-    [[ -d "${config_dir/$f}" || -f "${config_dir/$f}" ]] && mv "${config_dir/$f}" "${backup_dir}"
-    ln -s "${dotfiles_dir/$f}" "${config_dir/$f}"
+    [[ -d "${config_dir}/${f}" || -f "${config_dir}/${f}" ]] && mv "${config_dir}/${f}" "${backup_dir}"
+    ln -s "${dotfiles_dir}/${f}" "${config_dir}/${f}"
 done
