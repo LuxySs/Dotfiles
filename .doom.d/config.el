bf56c57a85ef2871bf4b06b7f0b3771efb098c44
Lucas <lucas.verbeiren@gmail.com>
2023-11-21 18:01:37 +0000

    dracula theme emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,7 +3,7 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-(setq doom-theme 'dracula)
+(setq doom-theme 'doom-dracula)
 
 (require 'multiple-cursors)
 
@@ -46,7 +46,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
