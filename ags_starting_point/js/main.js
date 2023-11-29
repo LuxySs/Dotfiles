58df88625a3540498475c92b2135118bb7b2c350
Lucas <lucas.verbeiren@gmail.com>
2023-11-29 21:48:21 +0000

    ags config (mine and the one I started with)

--- ags_starting_point/js/main.js
+++ ags_starting_point/js/main.js
@@ -1,33 +1,33 @@
 import { USER } from 'resource:///com/github/Aylur/ags/utils.js';
 import Bar from './bar/Bar.js';
-// import Notifications from './notifications/Notifications.js';
-// import ControlCenter from './controlCenter/ControlCenter.js';
-// import Applauncher from './applauncher/Applauncher.js';
+import Notifications from './notifications/Notifications.js';
+import ControlCenter from './controlCenter/ControlCenter.js';
+import Applauncher from './applauncher/Applauncher.js';
 import OSD from './osd/OSD.js';
 
 import * as setup from './utils.js';
 import { forMonitors } from './utils.js';
 import Calendar from './calendar/Calendar.js';
-// import { BluetoothDevices } from './controlCenter/widgets/Bluetooth.js';
-// import { WifiSelection } from './controlCenter/widgets/Network.js';
-// import { AppMixer } from './controlCenter/widgets/Volume.js';
-// import { SinkSelector } from './controlCenter/widgets/Volume.js';
+import { BluetoothDevices } from './controlCenter/widgets/Bluetooth.js';
+import { WifiSelection } from './controlCenter/widgets/Network.js';
+import { AppMixer } from './controlCenter/widgets/Volume.js';
+import { SinkSelector } from './controlCenter/widgets/Volume.js';
 
 setup.warnOnLowBattery();
-// setup.reloadCss();
+setup.reloadCss();
 setup.globalServices();
 
 const windows = () => [
     forMonitors(Bar),
-    // forMonitors(Notifications),
+    forMonitors(Notifications),
     forMonitors(OSD),
-    // ControlCenter(),
-    // Applauncher(),
+    ControlCenter(),
+    Applauncher(),
     Calendar(),
-    // BluetoothDevices(),
-    // WifiSelection(),
-    // AppMixer(),
-    // SinkSelector(),
+    BluetoothDevices(),
+    WifiSelection(),
+    AppMixer(),
+    SinkSelector(),
 ];
 
 export default {
