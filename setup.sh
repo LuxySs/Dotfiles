f07c246a5e7e3eadf9f60219130a44f7c30cad51
Lucas <lucas.verbeiren@gmail.com>
2023-12-09 18:00:01 +0000

    updated wlogout and swaylock to work together

--- setup.sh
+++ setup.sh
@@ -33,7 +33,7 @@ rm -rf "${backup_dir}" # make sure that it's empty before backing up
 mkdir "${backup_dir}" # create backup directory
 
 # for files that should be in .config
-for f in ags alacritty cava fish hypr neofetch nvim rofi starship.toml wlogout; do
+for f in ags alacritty cava fish hypr neofetch nvim rofi starship.toml swaylock wlogout; do
     [[ -d "${config_dir}/${f}" || -f "${config_dir}/${f}" ]] && mv "${config_dir}/${f}" "${backup_dir}" # move previous configs to backup directory
     ln -s "${dotfiles_dir}/${f}" "${config_dir}/${f}" # create the symlink
 done
@@ -45,3 +45,4 @@ ln -s "${dotfiles_dir}/.doom.d" "${HOME}" # create the symlink with .doom.d dire
 # .bashrc 
 [[ -f "${HOME}/.bashrc" ]] && mv "${HOME}/.bashrc" "${backup_dir}" # move previous doom config to backup directory
 ln -s "${dotfiles_dir}/.bashrc" "${HOME}" # create the symlink with .bashrc directory
+
