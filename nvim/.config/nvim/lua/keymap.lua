47d04ce0f27ae13e5f3023ef257ad4fb053c80dd
Lucas <lucas.verbeiren@gmail.com>
2023-12-29 15:30:29 +0000

    nvim config improvements

---
+++ nvim/.config/nvim/lua/keymap.lua
@@ -0,0 +1,12 @@
+local keymap = vim.keymap
+
+keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Navigate vim panes better
+keymap.set('n', '<c-k>', ':wincmd k<CR>')
+keymap.set('n', '<c-j>', ':wincmd j<CR>')
+keymap.set('n', '<c-h>', ':wincmd h<CR>')
+keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>')
+
---
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,6 @@
+return {
+    	"lervag/vimtex",
+    	init = function()
+    	end,
+    	ft = { "tex" }, -- Specify the filetypes for which VimTeX should be enabled
+}
