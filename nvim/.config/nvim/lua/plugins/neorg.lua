f1bcc5f7a49e28e0910dd3324c117cd72cf83abe
Lucas <lucas.verbeiren@gmail.com>
2024-01-04 14:03:09 +0100

    neorg plugin (added)

---
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,24 @@
+return {
+    "nvim-neorg/neorg",
+    run = ":Neorg sync-parsers", -- This is the important bit!
+    config = function()
+    require('neorg').setup {
+        load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {
+                config = {
+                    folds = true,
+                    icon_preset = "varied"
+                },
+            },
+            ["core.summary"] = {},
+            ["core.dirman"] = {
+                config = {
+                    workspaces = {
+                    }
+                }
+            }
+        }
+    }
+    end,
+}
