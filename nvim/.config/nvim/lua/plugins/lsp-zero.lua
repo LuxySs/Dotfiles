55005038334afd8c51c7a87ae2cebe9587978813
Lucas <lucas.verbeiren@gmail.com>
2024-01-10 19:18:11 +0000

    fixed lsp-zero scroll completions suggestions

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -37,8 +37,8 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-          ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+          ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item()),
+          ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item()),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
