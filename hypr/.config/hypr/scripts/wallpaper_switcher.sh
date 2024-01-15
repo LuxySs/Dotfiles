a2f1331b18cc7efe180dd3fdeea2c4434425a1e0
Lucas <lucas.verbeiren@gmail.com>
2024-01-15 10:51:21 +0000

    deleted useless wallpaper_switcher

--- hypr/.config/hypr/scripts/wallpaper_switcher.sh
+++
@@ -1,4 +0,0 @@
-hyprctl hyprpaper wallpaper "DP-1,~/wallpapers/gas_station.jpg" & hyprctl hyprpaper wallpaper "DP-2,~/wallpapers/gas_station.jpg"
-
-hyprctl hyprpaper wallpaper "DP-1,~/wallpapers/lake_moutains.jpg" & hyprctl hyprpaper wallpaper "DP-2,~/wallpapers/lake_moutains.jpg"
-
