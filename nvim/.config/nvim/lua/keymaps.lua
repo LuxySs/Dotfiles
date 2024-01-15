7444c124f9f2bc5a62880da5a41e27b15a604e33
Lucas <lucas.verbeiren@gmail.com>
2024-01-15 22:13:55 +0100

    swapped keybinds to free ldr e for oil

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -11,6 +11,8 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 -- Alpha menu
 vim.keymap.set('n', '<leader>a', ':Alpha<CR>')
+-- Start oil (file manager)
+vim.keymap.set('n', '<leader>e', ':Oil<CR>')
 
 
 
@@ -25,7 +27,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
