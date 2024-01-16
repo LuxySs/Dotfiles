ca80929fe65cdc114af9375bdb40ed796e104462
Lucas <lucas.verbeiren@gmail.com>
2024-01-16 20:56:24 +0100

    added blanks where needed

--- nvim/.config/nvim/lua/custom/plugins/alpha.lua
+++ nvim/.config/nvim/lua/custom/plugins/alpha.lua
@@ -23,9 +23,9 @@ return {
         }
         dashboard.section.buttons.val = {
             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("t", " Explore", ":Neotree toggle<CR>"),
-            dashboard.button("b", " Buffers", ":Telescope buffers<CR>"),
-            dashboard.button("c", " Colorscheme", ":Telescope colorscheme<CR>"),
+            dashboard.button("t", "  Explore", ":Neotree toggle<CR>"),
+            dashboard.button("b", "  Buffers", ":Telescope buffers<CR>"),
+            dashboard.button("c", "  Colorscheme", ":Telescope colorscheme<CR>"),
             dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
         }
         _Gopts = {
