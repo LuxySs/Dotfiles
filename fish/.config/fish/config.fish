450580e58360a3df5aac5bbb43a58dc0f849160a
Lucas <lucas.verbeiren@gmail.com>
2024-03-12 21:27:24 +0000

    multi file fish shell config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/functions.fish
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
 end
@@ -19,61 +22,3 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x CARGO_HOME $HOME/.cargo
 set -x RUSTUP_HOME $HOME/.rustup
 
-
-#### Functions ###
-# copy the current directory quickly
-function pwdc
-  set wd $(pwd)
-  echo $wd && wl-copy $wd
-end
-
-# launch a fabric module using "fabric {name of the module}"
-function fabric
-  set module $argv[1] 
-  source ~/fabric_stuff/venv/bin/activate.fish
-  python ~/.config/fabric/$module/$module.py
-end
-
-
-
-
-
-### ALIASES ###
-# funny garbage
-alias siuu="sudo pacman -Syu"
-
-# navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-
-# vim and emacs
-alias v='nvim'
-alias vim='nvim'
-alias fixvim='sudo rm -rf ~/.local/state/nvim/ && sudo rm -rf ~/.local/state/nvim/'
-alias em='/usr/bin/emacs -nw'
-alias emacs="emacsclient -c -a 'emacs'"
-alias rem="killall emacs || echo 'Emacs server not running'; /usr/bin/emacs --daemon"
-
-# "eza" instead of "ls" & "tree" 
-alias ls='eza --color=always'
-alias la='eza -a --color=always --group-directories-first'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first'  # long format
-alias lt='eza -aT --color=always --group-directories-first' # tree listing
-alias l.='eza -a | egrep "^\."'
-alias tree='eza --tree --color'
-
-# pacman & paru & yay
-alias pacsyu="sudo pacman -Syyu"	# update standard packages
-alias parusua="paru -sua"		# update aur packages
-alias parusua="paru -syu"		# update standard & aur packages
-alias yaysua="yay -sua --noconfirm"	# update aur packages
-alias yaysua="yay -syu --noconfirm"	# update standard & aur packages
-
-# Colorize grep output (good for log files)
-alias grep='grep --color=auto'
-
-# note taking
-alias note='~/.config/hypr/scripts/notes_screenshot/setup_resources.sh && vim'
---
+++ fish/.config/fish/functions.fish
@@ -0,0 +1,13 @@
+#### Functions ###
+# copy the current directory quickly
+function pwdc
+  set wd $(pwd)
+  echo $wd && wl-copy $wd
+end
+
+# launch a fabric module using "fabric {name of the module}"
+function fabric
+  set module $argv[1] 
+  source ~/fabric_stuff/venv/bin/activate.fish
+  python ~/.config/fabric/$module/$module.py
+end
