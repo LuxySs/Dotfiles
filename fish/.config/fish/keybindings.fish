7aa0cdbc278a9d751eb164200a16dc53978a34e1
Lucas <lucas.verbeiren@gmail.com>
2024-03-28 23:01:21 +0000

    other hyprland stuff / scripts

--- fish/.config/fish/keybindings.fish
+++ fish/.config/fish/keybindings.fish
@@ -1,2 +1,14 @@
-bind \cs 'bash ~/.config/fish/fuzzy_edit.sh'
-#bind \ch 'fzf --walker-root=$HOME'
+bind \cs fuzzy_here
+bind \ch fuzzy_home
+
+# searches from root by default
+function fuzzy_home
+  # Run the fuzzy_edit.sh script and store the exit status
+  ~/.config/hypr/scripts/fuzzy_edit.sh
+end
+
+# pass it an argument to change root
+function fuzzy_here
+  ~/.config/hypr/scripts/fuzzy_edit.sh $pwd
+end
+
